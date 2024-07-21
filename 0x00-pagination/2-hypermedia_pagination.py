22cdd90f872a76f94e0b2fd3dfd8bf92d8d6cd26
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-21 11:04:28 +0000

    Trying new techniques to make it work

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -7,12 +7,13 @@ import math
 """ File importation path """
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """ File executable path """
 
     begining_index = (page - 1) * page_size
     end_index = begining_index + page_size - 1
     return begining_index, end_index
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -33,7 +34,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page = 1, page_size = 10):
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """A method to validate the input data"""
         assert isinstance(page, int) and page > o
         assert isinstance(page_size, int) and page_size > 0
@@ -44,7 +45,7 @@ class Server:
             return []
         return dataset[begining_index:end_index]
 
-    def get_hyper(self, page = 1, page_size = 10):
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         assert isinstance(page, int) and page > 0,
         assert isinstance(page_size, int) and page_size > 0,
 
