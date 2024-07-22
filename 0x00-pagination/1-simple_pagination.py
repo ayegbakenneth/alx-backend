0990a8afb231604c7f5f5783d9cb6967e2daca10
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-22 15:21:50 +0000

    A new correction on Simple helper function

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -11,12 +11,13 @@ def index_range(page, page_size):
     """ File executable path """
 
     begining_index = (page - 1) * page_size
-    end_index = begining_index + page_size - 1
+    end_index = begining_index + page_size
     return begining_index, end_index
 
 
 class Server:
     """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -24,6 +25,7 @@ class Server:
 
     def dataset(self) -> List[List]:
         """Cached dataset"""
+
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
