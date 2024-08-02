0082d42e30c236a4ff09ccfe9041fec7b154f590
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-02 11:12:16 +0000

    Making new corrections

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,5 +12,7 @@ app = Flask(__name__)
 def home():
     """ A method that direct to the home page """
     return render_template('0-index.html')
+
+
 if __name__ == '__main__':
     app.run(debug = True)
