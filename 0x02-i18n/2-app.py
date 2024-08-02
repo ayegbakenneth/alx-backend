211027280862f09b729642142a7b3da14fbd495c
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-02 11:32:37 +0000

    Making new correction on the codes

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,6 +7,7 @@ from flask_babel import Babel
 
 
 app = Flask(__name__)
+
 babel = Babel(app)
 
 
@@ -16,7 +17,9 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
@@ -24,10 +27,12 @@ def get_locale():
     with our supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route("/")
 def home():
     """ A method that return the home page. """
     return render_template('2-index.html')
+
 
 if __name__ = ('__main__'):
     app.run(debug=True)
